4840b7adb216e27fdbc7971103e7618790525e9b
Simon van Dyk <simon.vandyk@gmail.com>
2019-01-10 16:03:56 +0000

    Add: base and landing page styling

--- lib/nada_web/controllers/page_controller.ex
+++ lib/nada_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule NadaWeb.PageController do
   use NadaWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", hide_navigation: true)
   end
 end
