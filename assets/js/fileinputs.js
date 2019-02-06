0cde578032361bd2083818f6f04f402306ac288c
Simon van Dyk <simon.vandyk@gmail.com>
2019-02-06 23:45:40 +0200

    Refactor fileinputs.js

--- assets/js/fileinputs.js
+++ assets/js/fileinputs.js
@@ -1,10 +1,31 @@
+const getFile = target => {
+  if (target.files.length === 0) {
+    return false;
+  } else {
+    return target.files[0];
+  }
+};
+
+const updateLabelText = (target, file) => {
+  let label = target.previousElementSibling;
+
+  if (file) {
+    label.innerText = file.name;
+  } else {
+    label.innerText = "Take a selfie / Choose a photo";
+  }
+};
+
 const setupFileInputs = () => {
   let fileInputs = document.querySelectorAll("input[type='file']");
   fileInputs.forEach(fileInput => {
     fileInput.addEventListener("change", ({ target }) => {
-      let fileName = target.files[0].name;
-      let label = target.previousElementSibling;
-      label.innerText = fileName;
+      let file = getFile(target);
+      updateLabelText(target, file);
+      if (!file) {
+        return;
+      }
+      //compressImage(target, file);
     });
   });
 };
